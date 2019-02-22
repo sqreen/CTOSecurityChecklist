aa48e0b347700c4a862972149ca276838e3c641c
Fx <kingeek@live.fr>
2019-02-22 18:54:29 +0100

    Update gulpfile + package

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,51 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pump = require('pump');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
-var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
-var autoprefixer = require('gulp-autoprefixer');
+const { src, dest, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const minifyCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync');
+const useref = require('gulp-useref');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Tasks -------------------------------------------------------------------- >
-gulp.task('styles', function () {
-  gulp.src('./docs/scss/style.scss')
+function styles() {
+  return src('./docs/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('./docs/css'))
+    .pipe(dest('./docs/css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./build/css/'))
+    .pipe(dest('./build/css/'))
     .pipe(browserSync.reload({ stream: true }));
-});
+};
 
-gulp.task('scripts', function () {
-  gulp.src('./docs/js/**/*.*')
-    .pipe(gulp.dest('./build/js'));
-});
+function scripts() {
+  return src('./docs/js/**/*.*')
+    .pipe(dest('./build/js'));
+};
 
-gulp.task('serve', function () {
+function serve() {
   browserSync({
     server: {
       baseDir: "./docs/"
     }
   });
-});
+};
 
-gulp.task('html', function () {
-  gulp.src('./docs/index.html')
+function html() {
+  return src('./docs/index.html')
     .pipe(useref())
-    .pipe(gulp.dest('./build/'));
-});
+    .pipe(dest('./build/'));
+};
 
+function img() {
+  return src('./docs/images/**/*.*')
+    .pipe(dest('./build/images'));
+};
 
-gulp.task('img', function () {
-  gulp.src('./docs/images/**/*.*')
-    .pipe(gulp.dest('./build/images'));
-});
+function fonts() {
+  return src('./docs/fonts/**/*.*')
+    .pipe(dest('./build/fonts'));
+};
 
-gulp.task('fonts', function () {
-  gulp.src('./docs/fonts/**/*.*')
-    .pipe(gulp.dest('./build/fonts'));
-});
-
-gulp.task('build', ['html', 'styles', 'scripts', 'img', 'fonts'])
-
-// Run all Gulp tasks and serve application
-gulp.task('default', ['serve', 'build'], function () {
-  gulp.watch('docs/scss/**/*.scss', ['styles']);
-  gulp.watch('docs/*.html', browserSync.reload);
-  gulp.watch('docs/js/**/*.js', browserSync.reload);
-});
+exports.serve = parallel(serve, html, styles, scripts, img, fonts);
+exports.default = parallel(html, styles, scripts, img, fonts);
